b807cdfa8280364f7c579e0644853ad20d3785a9
FionaLovett <42168992+FionaLovett@users.noreply.github.com>
2023-03-17 21:41:34 +0100

    1.4

--- unleashed-slider-popup-loader.js
+++ unleashed-slider-popup-loader.js
@@ -1,9 +1,8 @@
+const GLOBAL_UNLEASHED_VERSION_CURRENT = typeof GLOBAL_UNLEASHED_VERSION !== "undefined" ? GLOBAL_UNLEASHED_VERSION : "1.4";
+const GLOBAL_UNLEASHED_CDN = "https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@" + GLOBAL_UNLEASHED_VERSION_CURRENT;
+let scriptname = GLOBAL_UNLEASHED_CDN + "/unleashed-slider-popup.js";
+
 document.addEventListener('DOMContentLoaded', function () {
-
-	const GLOBAL_UNLEASHED_VERSION_CURRENT = typeof GLOBAL_UNLEASHED_VERSION !== "undefined" ? GLOBAL_UNLEASHED_VERSION : "1.3";
-
-	const GLOBAL_UNLEASHED_CDN = "https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@" + GLOBAL_UNLEASHED_VERSION_CURRENT;
-	let scriptname = GLOBAL_UNLEASHED_CDN + "/unleashed-slider-popup.js";
 
 	if (!document.querySelector('script[src="' + scriptname + '"]')) {
 		// The script hasn't been loaded yet, so load it now
