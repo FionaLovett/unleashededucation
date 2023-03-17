8ee104e7c7c29ace6c245051a6f3b5d7173601d0
FionaLovett <42168992+FionaLovett@users.noreply.github.com>
2023-03-17 14:18:34 +0100

    Adding close button, filter, toggling function

--- unleashed-slider-popup.js
+++ unleashed-slider-popup.js
@@ -1,53 +1,57 @@
-openUnleashedSliderPopup = (id) => {
+const UNLEASHED_SLIDER_ACTIVE = true;
+document.addEventListener('DOMContentLoaded', function () {
+	function unleashedsliderpopupclose() {
+		modalbg.style.display = "none";
+		slidercontainer.style.display = "none";
+		slidercontainer.innerHTML = "";
+		elfsightscript.setAttribute("src", "");
+	}
 
-const fullid = "unleashed-slider-popup-" + id.toString();
-alert(fullid);
-	const popupelem = document.getElementById(fullid);
+	const modalbg = document.createElement("div");
+	const slidercontainer = document.createElement("div");
+	const elfsightscript = document.createElement("script");
+	const elfsightsrc = "https://apps.elfsight.com/p/platform.js";
+	document.body.appendChild(elfsightscript);
 
-	popupelem.style.display= "none";
-}
+	modalbg.setAttribute("id", "unleashed-modal-background");
+	modalbg.addEventListener("click", unleashedsliderpopupclose, false);
 
-document.addEventListener('DOMContentLoaded', function() {
-   const modalbg = document.createElement("div");
-   const slidercontainer = document.createElement("div");
-   const elfsightscript = document.createElement("script");
-   const elfsightsrc = "https://apps.elfsight.com/p/platform.js";
-   document.body.appendChild(elfsightscript);
-   
-   modalbg.setAttribute("id", "unleashed-modal-background");
-   modalbg.addEventListener("click", function() {
-	   modalbg.style.display = "none";
-	   slidercontainer.style.display = "none";
-	   slidercontainer.innerHTML = "";
-	   elfsightscript.setAttribute("src", "");
-   }, false);
-   
-   slidercontainer.setAttribute("id", "unleashed-slider-popup-container");
+	slidercontainer.setAttribute("id", "unleashed-slider-popup-container");
 
-   document.body.appendChild(modalbg);
-   document.body.appendChild(slidercontainer);
-   
-   const collection = document.getElementsByClassName("unleashed-slider-thumbnail");
-   
-	
-	for(let i=0;i<collection.length;i++) {
+	document.body.appendChild(modalbg);
+	document.body.appendChild(slidercontainer);
+
+	const closeicon = document.createElement("img");
+	closeicon.src = "https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@latest/close-outline.png";
+	closeicon.setAttribute("id", "unleashed-slider-close-button");
+	closeicon.addEventListener("click", unleashedsliderpopupclose, false);
+
+	const collection = document.getElementsByClassName("unleashed-slider-thumbnail");
+
+
+	for (let i = 0; i < collection.length; i++) {
 		const t = collection[i];
-		
+
 		t.setAttribute("id", "unleashed-slider-thumbnail-" + i);
-		t.addEventListener("click", function() {
-			modalbg.style.display = "block";
-			slidercontainer.style.display = "block";
-			
-			const e = document.createElement("div");
-			e.setAttribute("class", "elfsight-app-" + t.getAttribute("data-elfsightid"));
-			slidercontainer.appendChild(e);
-			elfsightscript.setAttribute("src", elfsightsrc);
-		}, false);
-		
+		if (UNLEASHED_SLIDER_ACTIVE) {
+			t.addEventListener("click", function () {
+				modalbg.style.display = "block";
+				slidercontainer.style.display = "block";
+
+				const e = document.createElement("div");
+				e.setAttribute("class", "elfsight-app-" + t.getAttribute("data-elfsightid"));
+				slidercontainer.appendChild(e);
+				slidercontainer.appendChild(closeicon);
+				elfsightscript.setAttribute("src", elfsightsrc);
+			}, false);
+		} else {
+			t.style.cursor = "unset";
+		}
+
 		const img = document.createElement("img");
 		img.setAttribute("src", t.getAttribute("data-img"));
 		t.appendChild(img);
-		
-		
+
 	}
+
 }, false);
