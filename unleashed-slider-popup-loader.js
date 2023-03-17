f490294792fb82d71d7b77665e386ad2d3c968a5
FionaLovett <42168992+FionaLovett@users.noreply.github.com>
2023-03-17 16:26:15 +0100

    Fixing loader

--- unleashed-slider-popup-loader.js
+++ unleashed-slider-popup-loader.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-
 	let len = $('script').filter(function () {
 		return ($(this).attr('src').indexOf("unleashed-slider-popup.js") >= 0);
 	}).length;
 
 	//if there are no scripts that match, the load it
 	if (len === 0) {
-		$.getScript("https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@latest/unleashed-slider-popup.js?v=6");
+		$.getScript("https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@latest/unleashed-slider-popup.js?v=7");
 	}
 });
