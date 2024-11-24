e2f80da71053c72da697b411c9336a2d5e6acbf5
Fiona Lovett <fiona@beiq.com>
2024-11-24 17:59:53 +0100

    debug

--- unleashed-slider-popup-loader.js
+++ unleashed-slider-popup-loader.js
@@ -1,14 +1,17 @@
-const GLOBAL_UNLEASHED_VERSION_CURRENT = typeof GLOBAL_UNLEASHED_VERSION !== "undefined" ? GLOBAL_UNLEASHED_VERSION : "1.4";
-const GLOBAL_UNLEASHED_CDN = "https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@" + GLOBAL_UNLEASHED_VERSION_CURRENT;
+const GLOBAL_UNLEASHED_VERSION_CURRENT =
+  typeof GLOBAL_UNLEASHED_VERSION !== "undefined"
+    ? GLOBAL_UNLEASHED_VERSION
+    : "1.5";
+const GLOBAL_UNLEASHED_CDN =
+  "https://cdn.jsdelivr.net/gh/fionalovett/unleashededucation@" +
+  GLOBAL_UNLEASHED_VERSION_CURRENT;
 let scriptname = GLOBAL_UNLEASHED_CDN + "/unleashed-slider-popup.js";
 
-document.addEventListener('DOMContentLoaded', function () {
-
-	if (!document.querySelector('script[src="' + scriptname + '"]')) {
-		// The script hasn't been loaded yet, so load it now
-		var script = document.createElement('script');
-		script.src = scriptname;
-		document.head.appendChild(script);
-	}
-
+document.addEventListener("DOMContentLoaded", function () {
+  if (!document.querySelector('script[src="' + scriptname + '"]')) {
+    // The script hasn't been loaded yet, so load it now
+    var script = document.createElement("script");
+    script.src = scriptname;
+    document.head.appendChild(script);
+  }
 });
