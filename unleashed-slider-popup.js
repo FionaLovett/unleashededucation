09270b36f6e3cfff9de50c1d4ba373b7a7b8af6b
Fiona Lovett <fiona@beiq.com>
2024-11-24 17:52:34 +0100

    Adding button capability

--- unleashed-slider-popup.js
+++ unleashed-slider-popup.js
@@ -1,20 +1,20 @@
 function unleashedsliderpopupclose() {
-	modalbg.style.display = "none";
-	slidercontainer.style.display = "none";
-	slidercontainer.innerHTML = "";
-	elfsightscript.setAttribute("src", "");
+  modalbg.style.display = "none";
+  slidercontainer.style.display = "none";
+  slidercontainer.innerHTML = "";
+  elfsightscript.setAttribute("src", "");
 }
 
-const cssId = 'unleashed-slider-css';  // you could encode the css path itself to generate id..
+const cssId = "unleashed-slider-css"; // you could encode the css path itself to generate id..
 if (!document.getElementById(cssId)) {
-	var head = document.getElementsByTagName('head')[0];
-	var link = document.createElement('link');
-	link.id = cssId;
-	link.rel = 'stylesheet';
-	link.type = 'text/css';
-	link.href = GLOBAL_UNLEASHED_CDN + '/unleashed-slider-popup.css';
-	link.media = 'all';
-	head.appendChild(link);
+  var head = document.getElementsByTagName("head")[0];
+  var link = document.createElement("link");
+  link.id = cssId;
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = GLOBAL_UNLEASHED_CDN + "/unleashed-slider-popup.css";
+  link.media = "all";
+  head.appendChild(link);
 }
 
 const modalbg = document.createElement("div");
@@ -36,31 +36,55 @@ closeicon.src = GLOBAL_UNLEASHED_CDN + "/close-outline.png";
 closeicon.setAttribute("id", "unleashed-slider-close-button");
 closeicon.addEventListener("click", unleashedsliderpopupclose, false);
 
-const collection = document.getElementsByClassName("unleashed-slider-thumbnail");
-
+const collection = document.getElementsByClassName(
+  "unleashed-slider-thumbnail"
+);
 
 for (let i = 0; i < collection.length; i++) {
-	const t = collection[i];
+  const t = collection[i];
 
-	t.setAttribute("id", "unleashed-slider-thumbnail-" + i);
-	t.innerHTML = ""; // necessary because Kajabi adds a &npsb;
-	if (typeof GLOBAL_UNLEASHED_DISABLE_POPUP !== "undefined" && GLOBAL_UNLEASHED_DISABLE_POPUP) {
-		t.style.cursor = "unset";
-	} else {
-		t.addEventListener("click", function () {
-			modalbg.style.display = "block";
-			slidercontainer.style.display = "block";
+  t.setAttribute("id", "unleashed-slider-thumbnail-" + i);
+  t.innerHTML = ""; // necessary because Kajabi adds a &npsb;
+  if (
+    typeof GLOBAL_UNLEASHED_DISABLE_POPUP !== "undefined" &&
+    GLOBAL_UNLEASHED_DISABLE_POPUP
+  ) {
+    t.style.cursor = "unset";
+  } else {
+    t.addEventListener(
+      "click",
+      function () {
+        modalbg.style.display = "block";
+        slidercontainer.style.display = "block";
 
-			const e = document.createElement("div");
-			e.setAttribute("class", "elfsight-app-" + t.getAttribute("data-elfsightid"));
-			slidercontainer.appendChild(e);
-			slidercontainer.appendChild(closeicon);
-			elfsightscript.setAttribute("src", elfsightsrc);
-		}, false);
-	}
+        const e = document.createElement("div");
+        e.setAttribute(
+          "class",
+          "elfsight-app-" + t.getAttribute("data-elfsightid")
+        );
+        slidercontainer.appendChild(e);
+        slidercontainer.appendChild(closeicon);
+        elfsightscript.setAttribute("src", elfsightsrc);
+      },
+      false
+    );
+  }
 
-	const img = document.createElement("img");
-	img.setAttribute("src", t.getAttribute("data-img"));
-	t.appendChild(img);
-
+  const dtImg = t.getAttribute("data-img");
+  if (dtImg) {
+    const img = document.createElement("img");
+    img.setAttribute("src", dtImg);
+    t.appendChild(img);
+  } else {
+    const a = document.createElement("a");
+    const aColor = t.getAttribute("data-color") || "#47aeb1";
+    const aText = t.getAttribute("data-text") || "See before/after";
+    a.setAttribute("class", "btn btn-small btn-solid btn-auto background-dark");
+    a.setAttribute(
+      "style",
+      `{background-color: ${aColor}, border-color: ${aColor}}`
+    );
+    a.setAttribute("innerText", aText);
+    t.appendChild(a);
+  }
 }
